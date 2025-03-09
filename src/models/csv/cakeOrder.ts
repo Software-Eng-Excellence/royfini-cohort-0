b490600c2e24630d7b65be5516aa04a73ae48fa1
royfini <138002571+royfini@users.noreply.github.com>
2025-03-09 09:33:31 +0200

    Feature/csv parser (#3)
    * add json parser functionality
    
    * modify json parser
    
    * add unit test for json parser
    
    * add xml parser functionality
    
    * add unit tes for xml parser
    
    * modify csv parser
    
    * add bookOrder and clothingOrder models
    
    * move models to models/csv
    
    * delete unnecessary file
    
    * delete some files

---
+++ src/models/csv/cakeOrder.ts
@@ -0,0 +1,20 @@
+interface CakeOrder {
+    id: string;
+    type: string;
+    flavor: string;
+    filling: string;
+    size: string;
+    layers: string;
+    frostingType: string;
+    frostingFlavor: string;
+    decorationType: string;
+    decorationColor: string;
+    customMessage: string;
+    shape: string;
+    allergies: string;
+    specialIngredients: string;
+    packagingType: string;
+    price: string;
+    quantity: string;
+  }
+  
---
+++ src/models/csv/clothingOrder.ts
@@ -0,0 +1,14 @@
+export interface ClothingOrder {
+    orderId: string;
+    clothingType: string;
+    size: string;
+    color: string;
+    material: string;
+    pattern: string;
+    brand: string;
+    gender: string;
+    packaging: string;
+    specialRequest: string;
+    price: string;
+    quantity: string;
+  }
